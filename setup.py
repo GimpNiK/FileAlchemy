b589766a6ae1b027a7236b7e2836dbe2199310b4
GimpNiK <131478999+GimpNiK@users.noreply.github.com>
2025-07-28 23:16:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
 
 setup(
     name="FileAlchemy",
@@ -9,14 +8,19 @@ setup(
     author="GimpNiK",
     author_email="reshfizika@mail.ru",
     description="Powerful Python library for file system operations and text processing",
-    long_description=long_description,
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/GimpNiK/FileAlchemy",
     packages=find_packages(),
+
+    keywords="python file system text-processing encoding chardet unix-commands",
+    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+         "Topic :: Utilities",
+        "Topic :: System :: Filesystems",
     ],
     python_requires=">=3.8",
     install_requires=[
